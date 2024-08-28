77eef57e2464c0302ee8a33b92824376cdef11bc
muhammadridho4 <ridhomalang066@gmail.com>
2024-08-28 09:51:51 +0700

    add

--- sample_app.py
+++ sample_app.py
@@ -4,6 +4,6 @@ from flask import render_template
 sample = Flask(__name__)
 @sample.route("/")
 def main():
-return render_template("index.html")
+    return render_template("index.html")
 if __name__ == "__main__":
-sample.run(host="0.0.0.0", port=5050)
+    sample.run(host="0.0.0.0", port=5050)
